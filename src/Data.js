5b92ce6278887e49d10f25dd67fa6cc17ef3530f
Nuhamin Gulilat <nuhamin.gulilat.7@gmail.com>
2020-01-30 03:21:42 +0300

    first commit

---
+++ src/Data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+    {
+        id:1,
+        title:"Lenovo",
+        img:" img/lenovo.jpg",
+        price:17000,
+        company:"Lenovo",
+        info:
+           "This monitor type is Lenovo Thinkpad p53. Its processor is 9th Gen xeon and core 8-core.Its operation system upto windows 10 or Ubuntu Linux . graphics is upto NVIDIA Quadro RTX 5000. Has total memory upto 128GB. Its total storage is upto 6TB.Also have battery 9WHr. It ha 2 USB ports.",
+        inCart: false,
+        count:0,
+        total:0},
+        {
+        id:2,
+        title:"Assus",
+        img:" img/assus.jpg",
+        price:15000,
+        company:"Assus",
+        info:
+           "This laptop type is assus X543UA . It has upto windows 10 and window 10 is most recomended for this brand laptop. The graphics is integrated intel UHD graphics 620. Has total memory upto 12GB. Its total storage is upto 512GB.Has optical drive Super Multi DVD. Its battery is 13WHR.",
+        inCart: false,
+        count:0,
+        total:0 
+    },
+    {
+        id:3,
+        title:"Dell",
+        img:" img/dell.jpg",
+        price:13000,
+        company:"Dell",
+        info:
+           "This laptop type is Dell. Has 15.6 inch HD LED display. Has 4GB RAM. It has upto windows 10 Pro for workstations and Linux. Has total memory upto 128GB. Its total storage is upto 1TB.Also have battery 10WHr. It ha 3 USB ports. Is 4th Gen core i5 with stareo speaker. Also has buil-in DVD-drive. ",
+        inCart: false,
+        count:0,
+        total:0 
+    },
+    {
+        id:4,
+        title:"Samsung",
+        img:" img/samsung.jpg",
+        price:17000,
+        company:"Samsung",
+        info:
+           "This laptop type is Samsung. Its processor is Intel core i7. It has windows 10. the graphics is shared. Has total memory upto 8GB. Its total storage is upto 568GB.Also have 720 pixel camera. It ha 2 USB ports. It display 15 inch. Weights 1.29kg",
+        inCart: false,
+        count:0,
+        total:0 
+    },
+    {
+        id:5,
+        title:"Hp",
+        img:" img/hp.jpg",
+        price:16000,
+        company:"Hp",
+        info:
+           "This laptop type is Hp. Its processor is AMD Ryzen 7 processor. It has upto windows 10 Pro for workstations and Linux. The graphics is AMD Radeon™ RX Vega 10 Graphics. Has total memory upto 256GB. Its total storage is upto 3TB.Also have battery 12WHr. It ha 2 USB ports.",
+        inCart: false,
+        count:0,
+        total:0 
+    },
+    {
+        id:6,
+        title:"Toshiba",
+        img:" img/toshiba.jpg ",
+        price:15000,
+        company:"Toshiba",
+        info:
+           "This laptop type is Toshiba. Its processor is 4th Gen intel Quad-Core i7. Has total memory upto 8GB. Its total storage is upto 1TB.Also have battery 7WHr. It ha 2 USB ports.",
+        inCart: false,
+        count:0,
+        total:0 
+    },
+    {
+        id:7,
+        title:"Apple",
+        img:" img/apple.jpg",
+        price:23000,
+        company:"Apple",
+        info:
+           "This laptop type is Apple. Its processor is 1.6GHz dual-core intel core i5. Has total memory upto 8GB. Its total storage is upto 128GB.Also have battery 13WHr. It ha 2 USB ports.",
+        inCart: false,
+        count:0,
+        total:0 
+    },
+    {
+        id:8,
+        title:"Accer",
+        img:" img/accer.jpg",
+        price:8000,
+        company:"Accer",
+        info:
+           "This laptop type is Accer. Its processor is 2nd Gen Core i7. Has total memory upto 4GB. Its total storage is upto 500GB.Also have battery 7WHr. It ha 2 USB ports. The operating system is free DOS.",
+        inCart: false,
+        count:0,
+        total:0 
+    }
+];
+export const detailProduct = 
+    {
+        id:1,
+        title:"Lenovo",
+        img:" img/lenovo.jpg",
+        price:"17,000 ETB",
+        company:"Lenovo",
+        info:
+           "This monitor type is Lenovo Thinkpad p53. Its processor is 9th Gen xeon and core 8-core.Its operation system upto windows 10 or Ubuntu Linux . graphics is upto NVIDIA Quadro RTX 5000. Has total memory upto 128GB. Its total storage is upto 6TB.Also have battery 9WHr. It ha 2 USB ports.",
+        inCart: false,
+        count:0,
+        total:0
+    };
+        
---
+++ src/context.js
@@ -0,0 +1,172 @@
+import React, { Component } from 'react';
+import {detailProduct, storeProducts} from './Data';
+
+
+const ProductContext = React.createContext();
+
+ class ProductProvider extends Component {
+     state={
+         products:[],
+         detailProduct:detailProduct,
+         cart:[],
+         modlaOpen:false,
+         modalProduct:detailProduct,
+         cartSubTotal:0,
+         cartTax:0,
+         cartTotal:0
+     };
+     componentDidMount(){
+         this.setProducts();
+     }
+     setProducts = () =>{
+         let products = [];
+         storeProducts.forEach(item=>{
+             const singleItem = {...item};
+             products = [...products,singleItem];
+         })
+         this.setState(()=>{
+             return {products}
+         })
+     }
+
+     getItem = (id) =>{
+         const product = this.state.products.find(item => item.id===id);
+         return product;
+     }
+
+     handleDetail = (id) =>{
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {detailProduct:product}
+        })
+     };
+     addToCart = (id) =>{
+       let products = [...this.state.products];
+       const index = products.indexOf(this.getItem(id));
+       const product = products[index];
+       product.inCart = true;
+       product.count = 1;
+       const price = product.price;
+       product.total = price;
+       this.setState(()=>{
+           return {products:products, cart:[...this.state.cart, product]};
+       },
+       ()=>{
+           this.addTotals();
+       }
+       );
+
+    };
+    openModla = id =>{
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {modalProduct:product,modalOpen:true}
+        });
+    };
+    closeModal = () =>{
+     this.setState(()=>{
+         return {modalOpen:false}
+     })
+    }
+    increament = (id)=>{
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item =>item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(()=>{
+            return{
+                cart:[...tempCart]
+            }
+        },()=>{this.addTotals();
+        });
+    };
+    decreament = (id)=>{
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item =>item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+        product.count = product.count - 1;
+
+        if(product.count === 0){
+            this.removeItem(id)
+        }
+        else{
+            product.total = product.count * product.price;
+            this.setState(()=>{
+                return{
+                    cart:[...tempCart]
+                }
+            },()=>{this.addTotals();
+            });
+        }
+    }
+    removeItem = (id)=>{
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !==id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(() =>{
+            return{
+                cart:[...tempCart],
+                products:[...tempProducts]
+            }
+        },()=>{
+            this.addTotals();
+        })
+    }
+    clearCart = ()=>{
+        this.setState(()=>{
+            return{cart:[]};
+        },()=>{
+            this.setProducts();
+            this.addTotals();
+        });
+    }
+    addTotals = ()=>{
+      let subTotal = 0;
+      this.state.cart.map(item =>(subTotal += item.total));
+      const tempTax = subTotal * 0.1;
+      const tax = parseFloat(tempTax.toFixed(2));
+      const total = subTotal + tax;
+      this.setState(()=>{
+          return{
+              cartSubTotal:subTotal,
+              cartTax:tax,
+              cartTotal:total
+          }
+      })
+    }
+    render() {
+        return (
+          <ProductContext.Provider value={{
+              ...this.state,
+              handleDetail:this.handleDetail,
+              addToCart:this.addToCart,
+              openModal:this.openModla,
+              closeModal:this.closeModal,
+              increament:this.increament,
+              decreament:this.decreament,
+              removeItem:this.removeItem,
+              clearCart:this.clearCart
+
+          }}>
+            {this.props.children}
+          </ProductContext.Provider>
+        )
+    }
+}
+const ProductConsumer = ProductContext.Consumer;
+export {ProductProvider, ProductConsumer}; 
